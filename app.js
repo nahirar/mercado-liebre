fa217963aaa957468fa059faedf762d8ea7f86a3
nahirar <arrejorinahir@gmail.com>
2021-04-29 10:11:27 -0300

    agregando mercado libre

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const publicPath = path.resolve(__dirname, 'public');
+
+app.listen(3050, () => {
+    console.log("Esta funcionando!")
+});
+
+app.use(express.static(publicPath));
+app.get('/', (req, res)=> {
+    res.sendFile(path.join(__dirname + '/views/home.html'))
+});
+
