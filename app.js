3e6dcb7c502b2ba34b71fc193d505d88e83d1e95
nahirar <arrejorinahir@gmail.com>
2021-05-13 23:38:39 -0300

    fixed al header más arreglos varios

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const app = express();
 const path = require('path');
 const publicPath = path.resolve(__dirname, 'public');
 
-app.listen(3050, () => {
-    console.log("Esta funcionando!")
+app.listen(3040, () => {
+    console.log("Esta funcionando en puerto 3040!")
 });
 
 app.use(express.static(publicPath));
@@ -14,4 +14,11 @@ app.get('/', (req, res)=> {
 
 app.get('/crear-Producto', (req, res)=> {
     res.sendFile(path.join(__dirname + '/views/product.html'))
+});
+app.get('/register', (req, res)=> {
+    res.sendFile(path.join(__dirname + '/views/register.html'))
+});
+
+app.get('/login', (req, res)=> {
+    res.sendFile(path.join(__dirname + '/views/login.html'))
 });
